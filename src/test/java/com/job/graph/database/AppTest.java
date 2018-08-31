cdf7244fc71f6a5552bb9d0487a872afac16f8ff
toryzhou <522617505@qq.com>
2018-08-31 21:01:17 +0800

    Add tinkerpop

--- src/test/java/com/job/graph/database/AppTest.java
+++ src/test/java/com/job/graph/database/AppTest.java
@@ -1,4 +1,22 @@
 package com.job.graph.database;
 
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AppTest {
+
+    @Test
+    public void test() {
+        Graph graph = new TinkerGraph();
+        Vertex a = graph.addVertex("a");
+        Vertex b = graph.addVertex("b");
+
+        Edge like = a.addEdge("like", b);
+        Assert.assertEquals("like", like.getLabel());
+    }
 }
