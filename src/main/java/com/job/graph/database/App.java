877df80f608f1cfa19a6590667050317a11d7a12
torychow <522617505@qq.com>
2018-09-10 23:55:20 +0800

    Add spring boot

--- src/main/java/com/job/graph/database/App.java
+++ src/main/java/com/job/graph/database/App.java
@@ -1,4 +1,18 @@
 package com.job.graph.database;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(scanBasePackages = {"com.job.graph.database"})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class App {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(App.class);
+        application.setBannerMode(Banner.Mode.CONSOLE);
+        application.run(args);
+    }
 }
--- src/main/java/com/job/graph/database/service/TenementService.java
+++ src/main/java/com/job/graph/database/service/TenementService.java
@@ -1,11 +1,24 @@
 package com.job.graph.database.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class TenementService {
 
-
+    @Autowired
     private RestTemplate restTemplate;
 
-    public void test(){
-
+    public String get() {
+        String uri = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        ResponseEntity<String> exchange = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(httpHeaders), String.class);
+        return exchange.getBody();
     }
+
 }
