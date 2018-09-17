3255bd3c042b329da2ec713b7a2be820162bdb9b
toryzhou <522617505@qq.com>
2018-09-17 19:07:21 +0800

    Add Single graph

--- src/main/java/com/job/graph/database/dao/SingleGraph.java
+++ src/main/java/com/job/graph/database/dao/SingleGraph.java
@@ -1,18 +1,27 @@
 package com.job.graph.database.dao;
 
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
+
 /**
  * @author : toryzhou
  * @date: 9/13/18 19:45
  */
 public class SingleGraph {
 
-    private SingleGraph singleGraph = new SingleGraph();
+    private static SingleGraph singleGraph = new SingleGraph();
+
+    private Graph graph;
 
     private SingleGraph() {
-
+        graph = new TinkerGraph();
     }
 
-    public SingleGraph getInstance(){
-        return null;
+    public static SingleGraph getInstance() {
+        return singleGraph;
+    }
+
+    public Graph getGraph() {
+        return graph;
     }
 }
