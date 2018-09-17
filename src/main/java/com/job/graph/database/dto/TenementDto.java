6eb5472a34a044cebffcdaae4d422e72c18fc2e1
toryzhou <522617505@qq.com>
2018-09-17 19:41:10 +0800

    Add tenement save

---
+++ src/main/java/com/job/graph/database/dto/TenementDto.java
@@ -0,0 +1,16 @@
+package com.job.graph.database.dto;
+
+import lombok.Data;
+
+/**
+ * @author : toryzhou
+ * @date: 9/17/18 19:18
+ */
+@Data
+public class TenementDto {
+
+
+    private String address;
+    private String owner;
+    private Integer price;
+}
--- src/main/java/com/job/graph/database/service/TenementService.java
+++ src/main/java/com/job/graph/database/service/TenementService.java
@@ -1,5 +1,10 @@
 package com.job.graph.database.service;
 
+import com.job.graph.database.dao.SingleGraph;
+import com.job.graph.database.dto.TenementDto;
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +19,17 @@ public class TenementService {
     @Autowired
     private RestTemplate restTemplate;
 
+    public void save() {
+        Graph graph = SingleGraph.getInstance().getGraph();
+        TenementDto tenementDto = new TenementDto();
+        tenementDto.setAddress("菁英公寓");
+        tenementDto.setOwner("王女士");
+        tenementDto.setPrice(1000);
+        graph.addVertex(tenementDto);
+        graph.addVertex("a");
+        Vertex a = graph.getVertex("a");
+    }
+
     public String get() {
         String uri = "https://su.58.com/chuzu/0/?key=%E8%8F%81%E8%8B%B1%E5%85%AC%E5%AF%93&PGTID=0d3090a7-0000-522a-1cac-68cef6495b14&ClickID=2";
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -24,7 +40,7 @@ public class TenementService {
         return body;
     }
 
-    public String get(String key){
+    public String get(String key) {
         return "";
     }
 
